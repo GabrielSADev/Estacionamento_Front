29af225460c268987883e50b0ee5651e7025f86f
Gabriel S. A <falconrei123@gmail.com>
2023-06-17 16:22:21 -0300

    input modelo add

--- estacionamento-front-client/src/views/modeloView/ModeloListaView.vue
+++ estacionamento-front-client/src/views/modeloView/ModeloListaView.vue
@@ -2,6 +2,12 @@
 
 <header>
     <div class="class_button">
+        <div class="divLabelModelo">
+            <label for="" id="labelMarcaModelo">Marca :</label>
+            <input type="text" name="Nome da Marca no Modelo" id="nomeMarcaModelo">
+            <label for="" id="labelModelo">Nome do Modelo :</label>
+            <input type="text" name="Nome do Modelo" id="nomeModelo">
+        </div>
         <button>Adicionar Modelo</button>
     </div>
 </header>
@@ -64,8 +70,48 @@
 </script>
 
 <style>
-#modelo{
-    background-color: rgb(193, 185, 185);
-}
-
+.class_button{
+        background-color: #4f04e7;
+        height: 10vh;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        gap: 2vw;
+        
+    }
+    button{
+        border-radius: 8px;
+        border: none;
+        padding: 0.7rem 0.8rem;
+        background-color: #ff914d;
+        color: #5e17eb;
+        font-weight: 750;
+    }
+    .divLabelModelo{
+        background-color: #5e17eb;
+        border-radius: 8px;
+        padding: 6px 10px;
+        display: flex;
+        justify-content: center;
+        gap: 1.5vw;
+        align-items: center;
+    }
+    #nomeModelo{
+        border-radius: 8px;
+        padding: 1.3% 0%;
+        border: none;
+    }
+    #nomeMarcaModelo{
+        border-radius: 8px;
+        padding: 1.3% 0%;
+        border: none;
+    }
+    #labelMarcaModelo{
+        color: #ff914d;
+        font-size: 1.6vw;
+    }
+    #labelModelo{
+        color: #ff914d;
+        font-size: 1.6vw;
+    }
 </style>
